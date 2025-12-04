fe1a42318ff65bc2fe226c3edaec10f30b6dbba1
RedR1ghtHand <4aikaman@gmail.com>
2025-12-04 14:06:42 +0200

    refactor(dependencies): change main discord.py lib to disnake;
    enhance get_message function to optionally return raw messages.

--- utils/messages.py
+++ utils/messages.py
@@ -1,13 +1,18 @@
 from settings import MESSAGES
 
 
-def get_message(path: str, **kwargs) -> str:
+def get_message(path: str, return_raw: bool = False, **kwargs):
     node = MESSAGES
     for key in path.split('.'):
         node = node.get(key)
         if node is None:
             return f"[missing message: {path}]"
+
+    if return_raw:
+        return node
+
     if isinstance(node, str):
         return node.format(**kwargs)
+        
     return node
     
